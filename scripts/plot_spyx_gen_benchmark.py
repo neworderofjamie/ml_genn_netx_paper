b254bc70dfb9b7f317bf40690c41fb1e6ba9875b
neworderofjamie <neworderofjamie@gmail.com>
2024-11-27 14:45:26 +0000

    use mlGeNN numbers

--- scripts/plot_spyx_gen_benchmark.py
+++ scripts/plot_spyx_gen_benchmark.py
@@ -12,10 +12,10 @@ from pandas import read_csv
 import plot_settings
 
 genn_peak_memory = {(256, 1): 502, (256, 2): 470, (256, 5): 468,
-                    (1024, 2): 970, (1024, 5): 968}
+                    (1024, 1): 972, (1024, 2): 970, (1024, 5): 968}
 
 # Load new GeNN data
-genn_df = load_data_frame(["dt", "num_hidden"], lambda p: p["augmentation"] == "plain" and p["seed"] == 1234 and p["num_hidden"] == 256,
+genn_df = load_data_frame(["dt", "num_hidden"], lambda p: p["augmentation"] == "plain" and p["seed"] == 1234,
                           path=os.path.join("..", "classifier"),
                           load_train=True, load_train_perf=True)
 
@@ -37,6 +37,9 @@ data = data.drop(columns=["num_hidden", "dt"])
 
 data = data.sort_values(by="Timestep [ms]", ascending=False)
 
+data["genn_peak_memory"] = data.apply(lambda f: genn_peak_memory[(f["Num hidden"], f["Timestep [ms]"])],
+                                      axis="columns")
+
 data_1024_hidden = data[data["Num hidden"] == 1024]
 data_256_hidden = data[data["Num hidden"] == 256]
 
@@ -45,10 +48,10 @@ fig, axes = plt.subplots(1, 2, frameon=False, figsize=(plot_settings.column_widt
 
 # Plot memory
 actor_1024 = axes[0].plot(1000.0 / data_1024_hidden["Timestep [ms]"], 
-                          data_1024_hidden["Thomas peak GPU [mb]"] - data_1024_hidden["Thomas dataset GPU [mb]"],
+                          data_1024_hidden["genn_peak_memory"],
                           marker="o")
 actor_256 = axes[0].plot(1000.0 / data_256_hidden["Timestep [ms]"], 
-                         data_256_hidden["Thomas peak GPU [mb]"] - data_256_hidden["Thomas dataset GPU [mb]"], 
+                         data_256_hidden["genn_peak_memory"],
                          marker="o")
 axes[0].plot(1000.0 / data_1024_hidden["Timestep [ms]"], 
              data_1024_hidden["Spyx platform peak GPU [mb]"] - data_1024_hidden["Spyx dataset GPU [mb]"],
@@ -58,7 +61,7 @@ axes[0].plot(1000.0 / data_256_hidden["Timestep [ms]"],
              marker="o", color=actor_256[0].get_color(), linestyle="--")
 
 axes[1].plot(1000.0 / data_1024_hidden["Timestep [ms]"], data_1024_hidden["train_gpu_time"] + data_1024_hidden["Thomas build load time [s]"], marker="o", color=actor_1024[0].get_color())
-axes[1].plot(1000.0 / data_256_hidden["Timestep [ms]"], data_256_hidden["train_gpu_time"] + data_256_hidden["Thomas build load time [s]"],, marker="o", color=actor_256[0].get_color())
+axes[1].plot(1000.0 / data_256_hidden["Timestep [ms]"], data_256_hidden["train_gpu_time"] + data_256_hidden["Thomas build load time [s]"], marker="o", color=actor_256[0].get_color())
 axes[1].plot(1000.0 / data_1024_hidden["Timestep [ms]"], data_1024_hidden["Spyx time default [s]"], marker="o", color=actor_1024[0].get_color(), linestyle="--")
 axes[1].plot(1000.0 / data_256_hidden["Timestep [ms]"], data_256_hidden["Spyx time default [s]"], marker="o", color=actor_256[0].get_color(), linestyle="--")
 
@@ -75,7 +78,7 @@ for a in axes:
     a.xaxis.grid(False)
 
 fig.legend([actor_256[0],  mlines.Line2D([],[], color="black"), actor_1024[0],mlines.Line2D([],[], linestyle="--", color="black")], 
-           ["256 hidden neurons", "GeNN", "1024 hidden neurons",  "Spyx"], 
+           ["256 hidden neurons", "mlGeNN", "1024 hidden neurons",  "Spyx"], 
            loc="lower center", ncol=2, frameon=False)
 
 fig.tight_layout(pad=0, rect=[0.0, 0.225, 1.0, 1.0])
